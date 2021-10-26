9a10058b6d53995a066419c71abff577838ced56
demhahero <ahmedsmail_91@yahoo.com>
2021-10-26 02:04:51 -0400

    resolve navigation

--- src/components/Home/connect-button/connect.component.js
+++ src/components/Home/connect-button/connect.component.js
@@ -1,40 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "../../Wallet/connectors";
 import "./connect.component.scss";
 
 export default function ConnectToWallet() {
-  const { active, account, activate, deactivate } = useWeb3React();
+  const { active, account, activate } = useWeb3React();
 
   async function connect() {
     try {
       await activate(injected);
+      localStorage.setItem("connected", true);
     } catch (ex) {
       console.log(ex);
     }
   }
 
-  async function disconnect() {
-    try {
-      deactivate();
-    } catch (ex) {
-      console.log(ex);
+  // async function disconnect() {
+  //   try {
+  //     deactivate();
+  //     localStorage.setItem("connected", false);
+  //   } catch (ex) {
+  //     console.log(ex);
+  //   }
+  // }
+
+  useEffect(() => {
+    if (typeof window.ethereum === "undefined") {
+      window.alert("Please install MetaMask");
     }
-  }
+
+    if (localStorage.getItem("connected") !== false) {
+      activate(injected);
+    }
+  }, [activate]);
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {active ? (
+      {active && localStorage.getItem("connected") ? (
         <div>
           <span>
             <b>{account}</b>
           </span>
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            onClick={disconnect}
-          >
-            Disconnect
-          </button>
         </div>
       ) : (
         <div>
